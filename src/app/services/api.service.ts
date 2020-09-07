a55c3e5ae8577377ef56d91391aed9d1560e5b8c
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-09-06 19:54:57 -0500

    finished services

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { IBeer } from '../i-beer';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class APIService {
+
+  constructor(private httpService: HttpClient) {}
+
+  async get(path: string, params?: HttpParams): Promise<IBeer[]> {
+  return this.httpService.get<IBeer[]>(path, {params}).toPromise();
+  }
+}
